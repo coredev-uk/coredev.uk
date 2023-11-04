226e00f3b1c25c04aef6b231e1da1bb80963497a
Core <64542347+coredev-uk@users.noreply.github.com>
2023-11-04 22:42:50 +0000

    d

--- components/TitleContainer.vue
+++ components/TitleContainer.vue
@@ -3,7 +3,7 @@
     <NuxtImg src="/images/icon.png" width="200" class="rounded-full" />
     <div class="h-full m-auto">
       <h1 class="text-white text-5xl">Core</h1>
-      <span class="text-white/50 text-2xl">core gae</span>
+      <span class="text-white/50 text-2xl">placeholder</span>
     </div>
   </div>
 </template>
