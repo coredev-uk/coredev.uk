5e689e01af89ee24884683b5f4cfb88ac2a7ea71
Core <64542347+coredev-uk@users.noreply.github.com>
2023-11-04 23:04:47 +0000

    Update social media links and fix console log parameter.

--- components/Socials.vue
+++ components/Socials.vue
@@ -2,10 +2,10 @@
   <div
     class="flex flex-col gap-5 absolute left-5 top-5 p-3 rounded content-center"
   >
-    <SocialsButton icon="fa-brands fa-discord" tooltip="Discord" link="/" />
-    <SocialsButton icon="fa-brands fa-github" tooltip="GitHub" link="/" />
-    <SocialsButton icon="fa-brands fa-linkedin" tooltip="LinkedIn" link="/" />
-    <SocialsButton icon="fa-brands fa-x-twitter" tooltip="X" link="/" />
+    <SocialsButton icon="fa-brands fa-discord" tooltip="Discord" link="///discord.com/users/546426958465073163" />
+    <SocialsButton icon="fa-brands fa-github" tooltip="GitHub" link="///github.com/coredev-uk" />
+    <SocialsButton icon="fa-brands fa-linkedin" tooltip="LinkedIn" link="///linkedin.com/in/paul-thompson-693114193" />
+    <SocialsButton icon="fa-brands fa-x-twitter" tooltip="X" link="///twitter.com/core_hdd" />
   </div>
 </template>
 
--- pages/projects/[pid].vue
+++ pages/projects/[pid].vue
@@ -8,7 +8,7 @@
 const route = useRoute()
 
 // When accessing /posts/1, route.params.id will be 1
-console.log(route.params.id)
+console.log(route.params.pid)
 </script>
 
 <style>
