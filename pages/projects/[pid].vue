34fd138a6b79afb6bd76e86e2e4e564ab78542b1
Core <64542347+coredev-uk@users.noreply.github.com>
2023-11-05 18:24:58 +0000

    Add project box component to project page

--- pages/projects/[pid].vue
+++ pages/projects/[pid].vue
@@ -1,11 +1,22 @@
 <template>
-  <div>
-    
+  <div class="grid p-2 place-items-center bg-zinc-800 min-h-screen ">
+    <ProjectBox :data="project"/>
   </div>
 </template>
 
 <script setup lang="ts">
 const route = useRoute()
+const project = {
+    title: 'Project Title',
+    description: 'Project Description',
+    image: 'https://cider.sh/cider.png',
+    link: 'https://github.com',
+    stats: {
+        stars: 100,
+        forks: 100,
+        issues: 100
+    }
+}
 
 // When accessing /posts/1, route.params.id will be 1
 console.log(route.params.pid)
