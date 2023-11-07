b9a5918717bf9abe160fe77b60dd74af5cc20645
Core <64542347+coredev-uk@users.noreply.github.com>
2023-11-07 19:02:06 +0000

    change head

--- pages/index.vue
+++ pages/index.vue
@@ -3,24 +3,62 @@
 const description = "Welcome to Core's personal website, where you can learn more about their interests, experiences, and achievements. Explore Core's journey and get in touch with them to connect and collaborate.";
 const title = "Core's Website";
 const url = "https://coredev.uk";
+
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+
+
+  title,
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: description,
+    },
+    {
+      hid: 'og:title',
+      property: 'og:title',
+      content: title,
+    },
+    {
+      hid: 'og:description',
+      property: 'og:description',
+      content: description,
+    },
+    {
+      hid: 'og:url',
+      property: 'og:url',
+      content: url,
+    },
+    {
+      hid: 'twitter:card',
+      property: 'twitter:card',
+      content: 'summary',
+    },
+    {
+      hid: 'twitter:title',
+      property: 'twitter:title',
+      content: title,
+    },
+    {
+      hid: 'twitter:description',
+      property: 'twitter:description',
+      content: description,
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico',
+    },
+  ],
+});
 </script>
 
 <template>
-    <head>
-        <title>{{ title }}</title>
-        <meta property="description" :content="title" />
-
-        <meta property="og:title" :content="title">
-        <meta property="og:description" :content="description" />
-        <meta property="og:url" :content="url">
-
-        <meta property="twitter:card" content="summary">
-        <meta property="twitter:title" :content="title">
-        <meta property="twitter:description" :content="description">
-        
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-    </head>
-
     <div class="min-h-screen grid place-items-center bg-background font-sans">
         <Socials />
         <TitleContainer />
