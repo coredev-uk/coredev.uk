a84ca8e3aa151caf54133e8116f8ee8fbb7d43be
Core <64542347+coredev-uk@users.noreply.github.com>
2023-11-07 18:43:54 +0000

    Add meta tags and update page title and description.

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+const description = "Welcome to Core's personal website, where you can learn more about their interests, experiences, and achievements. Explore Core's journey and get in touch with them to connect and collaborate.";
+const title = "Core's Website";
+const url = "https://coredev.uk";
+
+</script>
 
 <template>
-  <div class="min-h-screen grid place-items-center bg-background font-sans">
-    <Socials />
-    <TitleContainer />
-    <ProjectDropdown />
-  </div>
+    <head>
+        <title>{{ title }}</title>
+        <meta property="description" :content="title" />
+
+        <meta property="og:image" content="Link preview image URL">
+        <meta property="og:title" :content="title">
+        <meta property="og:description" :content="description" />
+        <meta property="og:url" :content="url">
+
+        <meta property="twitter:card" content="summary">
+        <meta property="twitter:title" :content="title">
+        <meta property="twitter:description" :content="description">
+
+    </head>
+
+    <div class="min-h-screen grid place-items-center bg-background font-sans">
+        <Socials />
+        <TitleContainer />
+        <ProjectDropdown />
+    </div>
 </template>
