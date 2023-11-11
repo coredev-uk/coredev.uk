9c1dff733b536cbfd314e80a67ee7ec16bc5002c
Core <64542347+coredev-uk@users.noreply.github.com>
2023-11-11 17:32:34 +0000

    ok

--- components/TitleContainer.vue
+++ components/TitleContainer.vue
@@ -3,7 +3,7 @@
         <NuxtImg src="/images/profile.gif" width="200" class="rounded-full border border-[#c04830] shadow shadow-accent hover:shadow-md hover:shadow-accent transition-shadow duration-200" />
         <div class="h-full m-auto w-full">
             <h1 class="mb-2"><glitched-writer text="Core" preset="encrypted" class="text-white text-5xl" appear /></h1>
-            <span class="w-60 block"><glitched-writer :text="phrases" :queue="{ interval: 5000, loop: true }" preset="encrypted" class="text-white/50 text-2xl" appear /></span>
+            <span class="w-80 block"><glitched-writer :text="phrases" :queue="{ interval: 5000, loop: true }" preset="encrypted" class="text-white/50 text-2xl" appear /></span>
         </div>
     </div>
 </template>
