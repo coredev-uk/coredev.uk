97274af9f02b4ae0b040218cc0add72e36f67eaf
Core <core@coredev.uk>
2025-03-07 22:59:34 +0000

    feat: Add configuration for Prettier, ESLint, and Tailwind CSS; implement theme toggle and layout updates

---
+++ src/content/config.ts
@@ -0,0 +1,39 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+    type: "content",
+    // Type-check frontmatter using a schema
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+        heroImage: z.string().optional(),
+        tags: z.string().array().optional(),
+    }),
+});
+
+const project = defineCollection({
+    type: "content",
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        tech: z.string().array().optional(),
+        github: z.string().optional(),
+        url: z.string().optional(),
+        loadFactor: z.number(),
+    }),
+});
+
+const experience = defineCollection({
+    type: "content",
+    schema: z.object({
+        startDate: z.coerce.date(),
+        endDate: z.coerce.date().optional(),
+        company: z.string(),
+        title: z.string(),
+        tech: z.string().array().optional(),
+    }),
+});
+
+export const collections = { blog, project, experience };
